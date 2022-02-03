ac84d264c3aa72050c7443152e93fe95152d4a50
Anson <ansfoong@gmail.com>
2022-02-03 04:24:09 -0700

    Implement basic button for logging in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
 import type { NextPage } from 'next';
+import { FaDiscord } from 'react-icons/fa';
+import styles from '../utils/styles/home.module.scss';
 
 const Home: NextPage = () => {
-  return <div className="page">Home Page</div>;
+  const handleLogin = () => {
+    window.location.href = 'http://localhost:3001/api/auth/discord';
+  };
+  return (
+    <div className="page aligned-center">
+      <div>
+        <button className={styles.button} onClick={handleLogin}>
+          <FaDiscord size={50} color="#7289DA" />
+          <span>Login with Discord</span>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
